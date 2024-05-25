2e2608718c9b7b5e422231e483c5a22d5c6c294c
Sumon Chandra <topu199041@gmail.com>
2024-05-25 15:39:40 +0600

    fix

--- api/index.js
+++ api/index.js
@@ -1,17 +1,25 @@
 import express from "express";
 import "dotenv/config";
 import { getUsers } from "./db.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("Hello World");
 });
 
 app.get("/users", async (req, res) => {
-  const users = await getUsers();
-  res.send(users);
+  try {
+    const users = await getUsers();
+    // console.log(users);
+    res.send(users);
+  } catch (error) {
+    res.status(500).send("Failed to get users");
+    console.log("Failed to get users ", error);
+  }
 });
 
 app.get("/users/:id", async (req, res) => {
